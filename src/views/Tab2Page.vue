593658356601f200e2dfce2bf6f98d0f59262cee
LANH27 <aleis720221.siul@gmail.com>
2024-05-17 20:45:53 -0600

    ⭐Cambios

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -6,30 +6,39 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+     <ion-content :fullscreen="true">
   <ion-fab vertical="bottom" horizontal="center" slot="fixed">
     <ion-fab-button @click="takePhoto()">
       <ion-icon :icon="camera"></ion-icon>
     </ion-fab-button>
   </ion-fab>
 </ion-content>
+    </ion-content>
   </ion-page>
+
+<ion-content>
+  <ion-grid>
+    <ion-row>
+      <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+        <ion-img :src="photo.webviewPath"></ion-img>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+  <!-- <ion-fab> markup  -->
+</ion-content>
+
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { camera, trash, close } from 'ionicons/icons';
-import {
-  IonPage,
-  IonHeader,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
-} from '@ionic/vue';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { camera } from 'ionicons/icons';
+
+const { photos, takePhoto } = usePhotoGallery();
 </script>
